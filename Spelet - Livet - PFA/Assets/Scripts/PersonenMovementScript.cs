630598eb8c350e0144b5b7920729856376d5f444
NoahHellwig <noahhellwig@hotmail.com>
2021-01-12 21:55:55 +0100

    Klar med Personen movement script

--- Spelet - Livet - PFA/Assets/Scripts/PersonenMovementScript.cs
+++ Spelet - Livet - PFA/Assets/Scripts/PersonenMovementScript.cs
@@ -4,30 +4,43 @@ using UnityEngine;
 
 public class PersonenMovementScript : MonoBehaviour
 {
-    // The target marker.
-    [SerializeField] 
+    // The target marker (needs to be choosen in Unity editor) 
+    [SerializeField]
     Transform target;
 
-    // Speed in units per sec.
+    // Maxspeed in units per sec.
     [SerializeField]
-    float maxspeed;
+    float maxSpeed = 7f; 
+    float minSpeed = 0f; 
 
-    private float speed; 
+    // Max-Distance before Script-Carrier/"Personen" stops 
+    [SerializeField]
+    float maxDistance = 20f; 
+    float minDistance = 0f; 
 
-    // Position of player (needs to be choosen in Unity editor) 
+    // Position of player/"Tanken" (needs to be choosen in Unity editor) 
     [SerializeField]
     Transform player;
 
-    
+
     void Update()
     {
         // Calculate distance between script-carrier and player. 
         float dist = Vector3.Distance(player.position, transform.position);
-        Debug.Log("Distance to other" + dist);
+        // Debug.Log("Distance to other: " + dist);
+
+
+
+        // Scale dist to speed - The closer dist the higher speed is to maxspeed.  
+        float speed = scale(minDistance, maxDistance, maxSpeed, minSpeed, dist);
+        Debug.Log("This is the scaled dist to speed: " + speed);
+
 
         // The step size is equal to speed times frame time.
-        float step = (maxspeed - dist) * Time.deltaTime;
+        float step = speed * Time.deltaTime;
 
+
+        // If step is a negative value, don't do any steps. Otherwise walk towards target
         if (step < 0)
         {
             transform.position = Vector3.MoveTowards(transform.position, target.position, 0f);
@@ -39,5 +52,18 @@ public class PersonenMovementScript : MonoBehaviour
 
         }
 
+
     }
+
+    // Scale function to map distance to speed
+    public float scale(float OldMin, float OldMax, float NewMin, float NewMax, float OldValue)
+    {
+
+        float OldRange = (OldMax - OldMin);
+        float NewRange = (NewMax - NewMin);
+        float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+
+        return (NewValue);
+    }
+
 }
---
+++ Spelet - Livet - PFA/Assets/Scripts/Scalescript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scalescript : MonoBehaviour
+{
+
+    public float testdist;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    float scaled = scale(0f, 100f, 0f, 10f, testdist);
+    Debug.Log("This is the scale of testdist: " + scaled); 
+    
+
+    }
+  
+
+    public float scale(float OldMin, float OldMax, float NewMin, float NewMax, float OldValue)
+    {
+
+    float OldRange = (OldMax - OldMin);
+    float NewRange = (NewMax - NewMin);
+    float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+
+    return (NewValue);
+    }
+}
+       
+
+
