c354b375694fae4be034f5cc7ee780d079509587
NoahHellwig <noahhellwig@hotmail.com>
2021-01-12 12:26:38 +0100

    Skapade Floor, Player, Avatar, Rörelse och Mappar
    Materials, Prefabs, Scenes, Scripts mappar.
    Rörelse script satt på "Tanken".

---
+++ Spelet - Livet - PFA/Assets/Scripts/SpelareMovementScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpelareMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    float PlayerSpeed = 20f;
+
+    [SerializeField]
+    float PlayerRotationSpeed = 100;
+
+    private Transform myPlayer;
+    private CharacterController myControllerPlayer;
+
+    public AudioSource source;
+
+    void Awake()
+    {
+        myPlayer = GetComponent<Transform>();
+        myControllerPlayer = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+        // rotera
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        myPlayer.Rotate(
+                        0f,
+                        PlayerRotationSpeed * Time.deltaTime * moveHorizontal,
+                        0f
+                        );
+        // flytta    
+        float moveVertical = Input.GetAxis("Vertical");
+        myControllerPlayer.SimpleMove(myPlayer.forward * PlayerSpeed * moveVertical);
+
+    }
+
+}
