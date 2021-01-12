a41a5cbb79ed1adcc8aa624a6f518727e82f66dc
NoahHellwig <noahhellwig@hotmail.com>
2021-01-12 13:52:46 +0100

    Skapade script som får Personen att gå mot "Targets"

---
+++ Spelet - Livet - PFA/Assets/Scripts/PersonenMovementScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonenMovementScript : MonoBehaviour
+{
+    // The target marker.
+    [SerializeField] 
+    Transform target;
+
+    // Speed in units per sec.
+    [SerializeField]
+    float speed;
+
+    void Update()
+    {
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        // Move our position a step closer to the target.
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
