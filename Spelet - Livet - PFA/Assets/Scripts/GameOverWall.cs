d12f1d659d48f4d683e63ac2fef5c785342cef5c
nealandin <75014196+nealandin@users.noreply.github.com>
2021-01-12 13:19:15 +0100

    GameOverWall script, Rigidbody på Personen-Avataren

---
+++ Spelet - Livet - PFA/Assets/Scripts/GameOverWall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverWall : MonoBehaviour
+{
+    public bool EndGame = false;
+
+  
+     private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Death") {
+            //Destroy (other.gameObject);
+            //source.Play (); 
+            //GameInfo.NumCoins--;
+            Debug.Log("Game Over");
+            Application.Quit();
+        }
+    }
+}
