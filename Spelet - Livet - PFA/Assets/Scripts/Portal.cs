2162710804468057a8d08b8228350f07c6da822d
elsabinder <71463073+elsabinder@users.noreply.github.com>
2021-01-14 16:01:08 +0100

    Elsas scen med startkit
    Dörr och kuber som ska synas genom den.

---
+++ Spelet - Livet - PFA/Assets/Scripts/Portal.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Portal : MonoBehaviour
+{
+    public Material[] materials;
+    
+    void Start()
+    {
+        foreach (var mat in materials) 
+        {
+        mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+        }
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        Debug.Log(other.name);
+    }
+
+    void OnTriggerStay(Collider other) 
+    {
+        //Anger att denna collider inte känner av något annat än Main Camera
+        if (other.name != "Battery"){
+            Debug.Log(other.name);
+            return;           
+        }
+
+        //Kolla vinkeln på dörren så det blir rätt > eller <
+        //Ändra till z-värde i spelet
+
+        //Sätter en int till materialet och jämför detta mot shader
+        if (transform.position.z < other.transform.position.z)
+        {
+            Debug.Log("Outside other world");
+            foreach (var mat in materials) 
+            {
+                mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+            }
+        } 
+        else
+        {
+            
+            Debug.Log("Inside other world");           
+            foreach (var mat in materials)
+            {
+                mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+            }
+        }   
+    }
+
+    void OnDestroy() {
+                 
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Spelet - Livet - PFA/Assets/Scripts/doorControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorControl : MonoBehaviour
+{
+    public float speed;
+    public float angle;
+    public Vector3 direction;
+    private GameObject battery;
+
+    
+    void Start()
+    {
+        angle = transform.eulerAngles.y;
+        //Tilldelar angle det värde, vad det än var, den hade vid uppstarten av programmet
+
+        battery = GameObject.Find("Battery");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //anger dörren utan att specificera då det ska gälla objektet som scriptet är knutet till - stämmer
+        float distance = Vector3.Distance(transform.position, battery.transform.position);
+        float maxDistance = 10.0f;
+        bool isNear = distance <= maxDistance;    
+
+        if (Mathf.Round(transform.eulerAngles.y) != angle)
+        {
+            transform.Rotate(direction * speed);
+        }
+
+        if (isNear != true)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            angle = 180;
+            direction = Vector3.up; //Vector3.up ger motsatt håll 
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            angle = 90;
+            direction = -Vector3.up; //Vector3.up ger motsatt håll 
+        }
+    }
+}
