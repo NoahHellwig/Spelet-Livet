b9ec24ed773496d1adc7d31f0b95c03105928ba3
nealandin <75014196+nealandin@users.noreply.github.com>
2021-01-17 20:06:14 +0100

    Ändringar i scripts för andra kameran

---
+++ Spelet - Livet - PFA/Assets/Scripts/SecondCameraWidth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondCameraWidth : MonoBehaviour
+{
+    public RectTransform rt;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        float width = Screen.width*2;
+        float height = rt.rect.height;
+
+        rt.sizeDelta = new Vector2(width, height);
+        
+    }
+}
