b623f98164823d5b496602615c297d764f3fb1cf
NoahHellwig <noahhellwig@hotmail.com>
2021-01-18 13:07:07 +0100

    På börjat WallMovementWithEnd script

---
+++ Spelet - Livet - PFA/Assets/Scripts/WallMovementWithEnd.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovementWithEnd : MonoBehaviour
+{
+
+    //private Rigidbody rbw; //a reference to the Rigidbody-component called "rb"
+    private float speedWall = 1.5f;
+
+   
+    // Update is called once per frame. Fixed update works better for physics stuff, Unity likes it better, will look smoother.
+    void Update()
+    {
+
+        if (GameInfo.avatarReachedEnd == true)
+        {
+            Debug.Log("Wall sees Avatar at the End");
+        }
+
+        //add forward force 
+        //rbw.AddForce(0, 0, forwardForceWall * Time.deltaTime); //adjusts the framerate so that it is the same for all computers
+            transform.position += transform.forward * Time.deltaTime * speedWall;
+
+
+    }
+}
