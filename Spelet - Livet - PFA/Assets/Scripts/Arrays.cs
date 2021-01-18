edcd4165bb0dfb84d8de2f6f1a2711dba206a752
j0551 <josefincarlen@gmail.com>
2021-01-18 10:54:46 +0100

    test script för att skapa en slutstory

---
+++ Spelet - Livet - PFA/Assets/Scripts/Arrays.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+
+    public GameObject[] portals;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        portals = GameObject.FindGameObjectsWithTag("Portal");
+
+        for (int i = 0; i < portals.Length; i++)
+        {
+            Debug.Log("You have to choose between " +i+ portals[i].name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Spelet - Livet - PFA/Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour
+
+{
+    
+    public GameObject portal1;
+    public GameObject portal2;
+    public GameObject finalPortal;
+
+
+    void Start()
+    {
+        //Sätter texten för GameObjects
+        portal1.name = "you partyed all night.";
+        portal2.name = "you slept all day.";
+        finalPortal.name = "until you didn't.";
+    }
+
+//Grundfärg på tröskel bara för test
+private Color m_oldColor = Color.white;
+
+//Aktiveras när du kliver på tröskeln
+    void OnTriggerEnter(Collider other)
+    {
+        Renderer render = GetComponent<Renderer>();
+        m_oldColor = render.material.color;
+        render.material.color = Color.green;
+        Debug.Log("Went through door" + ' ' + gameObject.name);
+    }
+
+//Aktiveras när du kliver av tröskeln
+    void OnTriggerExit(Collider other)
+    {
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = m_oldColor;
+    }
+
+        
+}
