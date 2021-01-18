a15d783596f9ac9503e2d71557316a9cc0d2ee93
nealandin <75014196+nealandin@users.noreply.github.com>
2021-01-18 14:48:38 +0100

    Ändringar i kamerascripts

---
+++ Spelet - Livet - PFA/Assets/Scripts/SecCameraBorderScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecCameraBorderScript : MonoBehaviour
+{
+    public RectTransform rt;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        float width = Screen.width*2;
+        float height = (Screen.height/4) + 10;
+        float xPos = 300f;
+        float yPos = Screen.height*0.15f;
+
+        //Debug.Log(yPos);
+        
+
+        rt.sizeDelta = new Vector2(width, height);
+        rt.anchoredPosition = new Vector2(xPos, yPos);
+    }
+}
