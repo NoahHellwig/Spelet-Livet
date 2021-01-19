19078ccf92a95c2023b71d2cd8f8643cddad1e10
elsabinder <71463073+elsabinder@users.noreply.github.com>
2021-01-19 09:59:35 +0100

    Palles portalvariant
    Lägger upp Palles portal i egen scen och har lagt till version anpassad för vårt spel i dörr 1 på min scen. Två olika PortalWindow på den med varsitt script, Enter och Exit.

---
+++ Spelet - Livet - PFA/Assets/Scripts/Enter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Enter : MonoBehaviour
+{
+    public static bool PlayerIsEntering = false; 
+    //public Renderer cube; 
+    public Material[] materials; 
+
+    void Start()
+    {
+        foreach (var mat in materials) 
+        {
+        mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+        Debug.Log("Detta då?");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") {
+            return;
+        }
+
+        Debug.Log("Trigger portal entering"); 
+
+        //Kollar om spelaren kommer från exit-hållet
+        if(PortalExit.PlayerIsExiting == true) {
+            // show old world
+            Debug.Log("Is exiting");
+            //cube.material.color = Color.red;
+            PortalExit.PlayerIsExiting = false;
+
+        //Om inte, sätts PlayerIsEntering till true, och     
+        } else {
+            PlayerIsEntering = true;
+            Debug.Log("Is entering"); 
+            //cube.material.color = Color.white;
+        }
+       
+    }
+
+    void OnDestroy() {
+                 
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+        }
+
+    }   
+     
+        
+    
+}
---
+++ Spelet - Livet - PFA/Assets/Scripts/PortalExit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalExit : MonoBehaviour
+{
+
+    public static bool PlayerIsExiting = false; 
+    public Renderer cube; 
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") {
+            return;
+        }
+
+      //      Debug.Log("Trigger portal exiting"); 
+        if(PortalEnter.PlayerIsEntering == true) {
+            // show old world
+            //Debug.Log("is entering"); 
+            cube.material.color = Color.black;
+            PortalEnter.PlayerIsEntering = false;
+        } else {
+            PlayerIsExiting = true;
+            //Debug.Log("can exit"); 
+            cube.material.color = Color.black;
+
+        }    
+    }
+}
