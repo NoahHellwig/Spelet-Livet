f51fc33b1bc18350bfc377c0e9a639e4a1e76841
j0551 <josefincarlen@gmail.com>
2021-01-19 13:49:04 +0100

    script som saknas

---
+++ Spelet - Livet - PFA/Assets/Scripts/PortalReview.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalReview : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") {
+            return;
+        }
+
+    foreach(string quest in PlayerMemory.Quests)
+        {
+            Debug.Log("" + quest);  
+        }
+
+    
+    }
+}
