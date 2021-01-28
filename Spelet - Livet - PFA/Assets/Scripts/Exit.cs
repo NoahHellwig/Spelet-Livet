e7147560105a2ce7161e8c7b7425267c4c4db37b
elsabinder <71463073+elsabinder@users.noreply.github.com>
2021-01-28 10:47:41 +0100

    Avaktivering av banor
    Nu finns grunden för avaktivering av banor i Exit-skriptet.

--- Spelet - Livet - PFA/Assets/Scripts/Exit.cs
+++ Spelet - Livet - PFA/Assets/Scripts/Exit.cs
@@ -12,9 +12,34 @@ public class Exit : MonoBehaviour
     GameObject myPlayer;
     PlayerHistory ph;
     public Material[] materialstwo;
+    public GameObject[] levelOne;
+    public GameObject[] levelTwo;
+    public GameObject[] levelThree;
+    public GameObject[] levelFour;
+    public GameObject[] levelFive;
+    public GameObject[] levelSix;
+    public GameObject[] levelSeven;
+    public GameObject[] levelEight;
+    public GameObject[] levelNine;
+    public GameObject[] levelTen;
+    public GameObject[] levelEleven;
+    public GameObject[] levelTwelve;
+    public GameObject[] levelThirteen;
+    public GameObject[] levelFourteen;
+    
+    
     void Start() {
+        //Hämta skriptet Playerhistory från spelaren
         myPlayer = GameObject.Find("Tanken - Spelaren");
         ph = myPlayer.GetComponent<PlayerHistory>();
+
+        //Hämta GameObjectArrayer för varje bana
+        levelOne = GameObject.FindGameObjectsWithTag("Level 1");
+        levelTwo = GameObject.FindGameObjectsWithTag("Level 2");
+        levelThree = GameObject.FindGameObjectsWithTag("Level 3");
+        levelFour = GameObject.FindGameObjectsWithTag("Level 4");
+        levelFive = GameObject.FindGameObjectsWithTag("Level 5");
+        levelSix = GameObject.FindGameObjectsWithTag("Level 6");
     }
 
     void OnTriggerEnter(Collider other)
@@ -25,11 +50,40 @@ public class Exit : MonoBehaviour
             return;
         }
 
-        //Hämta string med spelarens historik
-        
-        if (ph.History == "BB"){
-            Debug.Log("Booooooo");
+        //Använd string med spelarens historik        
+        if (ph.History == "A"){
+            
+            foreach(GameObject go in levelTwo) 
+            {
+                go.SetActive(false);
+                Debug.Log("Deaktiverar bana 2");
+            }
+            
         }
+        
+        if (ph.History == "B"){
+            
+            foreach(GameObject go in levelOne) 
+            {
+                go.SetActive(false);
+                Debug.Log("Deaktiverar bana 1");
+            }
+            
+        }
+
+
+        if (ph.History == "AA"){
+            
+            foreach(GameObject go in levelFour) 
+            {
+                go.SetActive(false);
+                Debug.Log("Deaktiverar bana 4");
+            }
+            
+        }
+
+        
+
 
         if(GameInfo.PlayerIsEntering == true) {
             //cube.material.color = Color.black;
