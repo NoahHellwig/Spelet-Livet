b881a3c4c5363d708101f439f6e05406810f0835
j0551 <josefincarlen@gmail.com>
2021-01-29 19:28:47 +0100

    Sluttext scripts 4 (en per val) i josefin scen #3

---
+++ Spelet - Livet - PFA/Assets/Scripts/PLAYER MEMORY 1-4/First door/endText1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class endText1 : MonoBehaviour
+{
+    public Text firstDoorText;
+
+    void Start()
+    {
+       firstDoorText.text = "";
+    }
+
+        void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") {
+            return;
+        }
+        
+        foreach(string quest in avatarMemoryFirstPortal.Quests)
+           {
+           firstDoorText.text = ("" + quest);  // Denna visar bara upp det senaste i listan
+         //  Debug.Log("" + quest + "VISAS DENNA PORT?"); // Denna visar alla objekt i listan
+}       
+
+
+    }
+
+    
+}
---
+++ Spelet - Livet - PFA/Assets/Scripts/PLAYER MEMORY 1-4/First door/portalQuest1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalQuest1 : MonoBehaviour
+{
+
+    public string PortalName; 
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") {
+            return;
+        }
+        avatarMemoryFirstPortal.Quests.Add(PortalName);   
+    }
+}
