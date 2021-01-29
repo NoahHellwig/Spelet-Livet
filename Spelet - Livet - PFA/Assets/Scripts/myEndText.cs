39b271d8e305696b1642796b1fe061419dbe8c2f
j0551 <josefincarlen@gmail.com>
2021-01-29 13:29:41 +0100

    Text visas på skärm

---
+++ Spelet - Livet - PFA/Assets/Scripts/myEndText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class myEndText : MonoBehaviour
+{
+    public Text endText;
+
+    void Start()
+    {
+       endText.text = "";
+    }
+
+    void Update ()
+    {
+
+    }
+
+
+        void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") {
+            return;
+        }
+        
+        foreach(string quest in PlayerMemory.Quests)
+           {
+           endText.text = ("" + quest);  // Denna visar bara upp det senaste i listan
+           Debug.Log("" + quest + "VISAS DENNA PORT?"); // Denna visar alla objekt i listan
+}       
+
+
+    }
+
+    
+}
