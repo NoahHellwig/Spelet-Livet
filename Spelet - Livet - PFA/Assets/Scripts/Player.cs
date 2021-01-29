452983ec165c26aac28891db20a0503c670db226
ejhemlig <75251341+ejhemlig@users.noreply.github.com>
2021-01-29 02:52:47 +0100

    spelar och kamerascript
    Kamerascriptet styr också spelaren men läggs på main camera och sedan droppar man player som target

---
+++ Spelet - Livet - PFA/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float movementSpeed = 10;
+    public float turningSpeed = 60;
+    Vector3 m_Movement;
+    Animator m_Animator;
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+
+    }
+    void Update() {
+        float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+        transform.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking",isWalking);
+
+    }
+}
