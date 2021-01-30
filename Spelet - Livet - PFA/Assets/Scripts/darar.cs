cb9a37c8af7af8c06590a042f296fe0bbea61dbd
ejhemlig <ejhemlig@protonmail.com>
2021-01-30 06:31:08 +0100

    En bättre preview på vad jag än så länge hunnit med

---
+++ Spelet - Livet - PFA/Assets/Scripts/darar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class darar : MonoBehaviour
+{
+    public bool open = false;
+    public float oppenVinkel = 90f;
+    public float stangdVinkel = 0f;
+    public float smooth = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void ChangeDoorState()
+    {
+        open = !open;
+    }
+    // Update is called once per frame
+    void Update(){
+    if(open)
+    {
+        Quaternion targetRotation = Quaternion.Euler(0,oppenVinkel,0);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation,targetRotation,smooth*Time.deltaTime);
+    }
+    else
+    {
+        Quaternion targetRotation2 = Quaternion.Euler(0,stangdVinkel,0);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation,targetRotation2,smooth*Time.deltaTime);
+    }}
+}
