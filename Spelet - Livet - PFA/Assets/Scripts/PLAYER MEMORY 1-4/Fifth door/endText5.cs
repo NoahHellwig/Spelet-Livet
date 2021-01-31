4ffd64fbd699267b88c80338b36582eebfd81b15
j0551 <josefincarlen@gmail.com>
2021-02-01 00:52:18 +0100

    Text till alla dörrar

---
+++ Spelet - Livet - PFA/Assets/Scripts/PLAYER MEMORY 1-4/Fifth door/endText5.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class endText5 : MonoBehaviour
+{
+    public Text fifthDoorText;
+
+    void Start()
+    {
+       fifthDoorText.text = "";
+    }
+
+        void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Avatar") {
+            return;
+        }
+        
+        foreach(string quest in avatarMemoryFifthPortal.Quests)
+           {
+           fifthDoorText.text = ("" + quest);  // Denna visar bara upp det senaste i listan
+           // Debug.Log("" + quest + "VISAS DENNA PORT?"); // Denna visar alla objekt i listan
+}       
+
+    }
+
+    
+}
+
+
+
